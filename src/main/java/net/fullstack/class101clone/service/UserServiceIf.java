6e0b8435003bbf410a696980abc09fca125555af
kmkang <wowkmini15@gmail.com>
2025-06-10 18:53:47 +0900

    [feat] delete authenticate method

--- src/main/java/net/fullstack/class101clone/service/UserServiceIf.java
+++ src/main/java/net/fullstack/class101clone/service/UserServiceIf.java
@@ -4,7 +4,6 @@ import net.fullstack.class101clone.domain.UserEntity;
 import net.fullstack.class101clone.dto.UserDTO;
 
 public interface UserServiceIf {
-	boolean authenticate(String userId, String userPwd);
 
 	UserEntity findByUserId(String userId);
 
