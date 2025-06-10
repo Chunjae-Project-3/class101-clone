0d1b6d00de1436cd2e5c9788f7ba59ece86ee474
kmkang <wowkmini15@gmail.com>
2025-06-10 15:45:32 +0900

    [refactor] remove unused imports in LoginController

--- src/main/java/net/fullstack/class101clone/controller/LoginController.java
+++ src/main/java/net/fullstack/class101clone/controller/LoginController.java
@@ -1,9 +1,7 @@
 package net.fullstack.class101clone.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
