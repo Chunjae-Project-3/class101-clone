d8941b12fbde5d043430d903a14cd24a61e15bda
kmkang <wowkmini15@gmail.com>
2025-06-11 00:41:12 +0900

    [refactor] delete unused import statement

--- src/main/java/net/fullstack/class101clone/config/SpringSecurityConfig.java
+++ src/main/java/net/fullstack/class101clone/config/SpringSecurityConfig.java
@@ -2,19 +2,16 @@ package net.fullstack.class101clone.config;
 
 import lombok.RequiredArgsConstructor;
 import net.fullstack.class101clone.repository.login.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
