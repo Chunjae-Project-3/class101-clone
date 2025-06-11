7498c23bc6af23bc99eda4826c76a9b306d98bac
kmkang <wowkmini15@gmail.com>
2025-06-11 11:44:38 +0900

    [feat] add UserRepositoryIfCustom interface for user management operations

---
+++ src/main/java/net/fullstack/class101clone/repository/login/UserRepositoryIfCustom.java
@@ -0,0 +1,17 @@
+package net.fullstack.class101clone.repository.login;
+
+import net.fullstack.class101clone.domain.UserEntity;
+import net.fullstack.class101clone.dto.UserDTO;
+
+public interface UserRepositoryIfCustom {
+	// 회원가입
+	boolean signup(UserDTO userDTO);
+	// 아이디 중복 체크
+	boolean existsByUserId(String userId);
+	// 탈퇴
+//	boolean deleteByUserId(String userId);
+	// 로그인
+	UserEntity login(UserDTO userDTO);
+	// 회원정보 수정
+
+}
