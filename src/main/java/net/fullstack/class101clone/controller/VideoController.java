ad60865bfb67e94112526bdbf5bca3b831efb90c
hyeon48615 <hyeon48615@gmail.com>
2025-06-11 16:46:21 +0900

    [feat] 비디오 플레이어 구현

---
+++ src/main/java/net/fullstack/class101clone/controller/VideoController.java
@@ -0,0 +1,17 @@
+package net.fullstack.class101clone.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Log4j2
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/video")
+@Tag(name = "동영상 스트리밍 API")
+public class VideoController {
+
+
+}
