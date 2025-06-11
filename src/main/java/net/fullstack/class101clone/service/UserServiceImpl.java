3f6b604ed12889938a0e6541afe1bc4b5672cf64
kmkang <wowkmini15@gmail.com>
2025-06-11 17:24:37 +0900

    [feat] add quit and updateUserInfo methods to UserServiceImpl

--- src/main/java/net/fullstack/class101clone/service/UserServiceImpl.java
+++ src/main/java/net/fullstack/class101clone/service/UserServiceImpl.java
@@ -1,10 +1,12 @@
 package net.fullstack.class101clone.service;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import net.fullstack.class101clone.domain.UserEntity;
 import net.fullstack.class101clone.dto.UserDTO;
 import net.fullstack.class101clone.repository.login.UserRepositoryIf;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -25,6 +27,11 @@ public class UserServiceImpl implements UserServiceIf {
 	}
 
 	@Override
+	public boolean quit(UserDTO userDTO) {
+		return userRepository.quit(userDTO.getUserId());
+	}
+
+	@Override
 	public boolean signup(UserDTO userDTO) {
 		return userRepository.signup(userDTO);
 	}
@@ -32,5 +39,26 @@ public class UserServiceImpl implements UserServiceIf {
 	@Override
 	public boolean existsByUserId(String userId) {
 		return userRepository.existsByUserId(userId);
+	}
+
+	@Override
+	@Transactional
+	public UserDTO updateUserInfo(UserDTO userDTO, String newPwd) {
+		// 로그인 방식으로 인증
+		UserEntity user = userRepository.login(userDTO);
+		if (user != null) {
+			// 새 비밀번호가 있으면 userDTO에 반영
+			if (newPwd != null && !newPwd.isEmpty()) {
+				userDTO.setUserPwd(newPwd);
+			}
+			UserEntity updatedUser = userRepository.updateUserInfo(userDTO);
+			if (updatedUser != null) {
+				return UserDTO.builder()
+						.userId(updatedUser.getUserId())
+						.userName(updatedUser.getUserName())
+						.build();
+			}
+		}
+		return null;
 	}
 }
