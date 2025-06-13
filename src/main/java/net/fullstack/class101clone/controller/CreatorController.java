05ccb58274e0c07a94bcf246c43bc21b4763866f
wlsidelw <109274960+wlsidelw@users.noreply.github.com>
2025-06-13 11:26:05 +0900

    [feat] CreatorController
    컨트롤러 껍데기만 만들어둠

---
+++ src/main/java/net/fullstack/class101clone/controller/CreatorController.java
@@ -0,0 +1,13 @@
+package net.fullstack.class101clone.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+
+@Log4j2
+@Controller
+@RequiredArgsConstructor
+public class CreatorController {
+
+}
