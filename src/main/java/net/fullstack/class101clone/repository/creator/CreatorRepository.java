38e2e0f447cabb0e5ee497b096751bc53e264ccf
wlsidelw <109274960+wlsidelw@users.noreply.github.com>
2025-06-14 23:42:48 +0900

    [feat] CreatorRepository
    리포지터리 커스텀 상속 적용

--- src/main/java/net/fullstack/class101clone/repository/creator/CreatorRepository.java
+++ src/main/java/net/fullstack/class101clone/repository/creator/CreatorRepository.java
@@ -3,6 +3,6 @@ package net.fullstack.class101clone.repository.creator;
 import net.fullstack.class101clone.domain.CreatorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CreatorRepository extends JpaRepository<CreatorEntity, Integer> {
+public interface CreatorRepository extends JpaRepository<CreatorEntity, Integer> , CreatorRepositoryCustom {
 
 }
