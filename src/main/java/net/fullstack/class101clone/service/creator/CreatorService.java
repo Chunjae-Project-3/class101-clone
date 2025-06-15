7e443d36de24100cfb7d353d1fc547059afaf44a
wlsidelw <109274960+wlsidelw@users.noreply.github.com>
2025-06-15 17:37:23 +0900

    [refactoring]CreatorService
    CreatorService 단일 파일 분리

--- src/main/java/net/fullstack/class101clone/service/creator/CreatorService.java
+++ src/main/java/net/fullstack/class101clone/service/creator/CreatorService.java
@@ -1,26 +1,12 @@
 package net.fullstack.class101clone.service.creator;
 
-import lombok.RequiredArgsConstructor;
 import net.fullstack.class101clone.domain.ClassEntity;
 import net.fullstack.class101clone.domain.CreatorEntity;
-import net.fullstack.class101clone.repository.creator.CreatorRepository;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import net.fullstack.class101clone.dto.ClassDTO;
 
 import java.util.List;
 
-@Service
-@RequiredArgsConstructor
-@Transactional(readOnly = true)
-public class CreatorService {
-    private final CreatorRepository creatorRepository;
-
-    public CreatorEntity getCreator(Integer creatorId) {
-        return creatorRepository.findById(creatorId)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 크리에이터. id :" + creatorId));
-    }
-
-    public List<ClassEntity> showRecentClassesOfCreator(Integer creatorId) {
-        return creatorRepository.recentClasses(creatorId);
-    }
+public interface CreatorService {
+    CreatorEntity getCreator(Integer creatorId);
+    List<ClassDTO> showRecentClassesOfCreator(Integer creatorId);
 }
