6ac7ba9a3aa335a24715de77829b6a79de6f63c9
kmkang <wowkmini15@gmail.com>
2025-06-17 13:52:28 +0900

    [fix] update session management in SpringSecurityConfig for improved security handling

--- src/main/java/net/fullstack/class101clone/config/SpringSecurityConfig.java
+++ src/main/java/net/fullstack/class101clone/config/SpringSecurityConfig.java
@@ -24,75 +24,82 @@ import java.util.Base64;
 @RequiredArgsConstructor
 @EnableMethodSecurity(prePostEnabled = true)
 public class SpringSecurityConfig {
-    private final CorsConfig corsConfig;
-    private final AuthenticationConfiguration authenticationConfiguration;
+	private final CorsConfig corsConfig;
+	private final AuthenticationConfiguration authenticationConfiguration;
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new PasswordEncoder() {
-            @Override
-            public String encode(CharSequence rawPassword) {
-                return hashWithSHA256(rawPassword.toString());
-            }
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new PasswordEncoder() {
+			@Override
+			public String encode(CharSequence rawPassword) {
+				return hashWithSHA256(rawPassword.toString());
+			}
 
-            @Override
-            public boolean matches(CharSequence rawPassword, String encodedPassword) {
-                String hashedAttemptedPassword = hashWithSHA256(rawPassword.toString());
-                return hashedAttemptedPassword.equals(encodedPassword);
-            }
+			@Override
+			public boolean matches(CharSequence rawPassword, String encodedPassword) {
+				String hashedAttemptedPassword = hashWithSHA256(rawPassword.toString());
+				return hashedAttemptedPassword.equals(encodedPassword);
+			}
 
-            private String hashWithSHA256(String input) {
-                try {
-                    MessageDigest md = MessageDigest.getInstance("SHA-256");
-                    byte[] hash = md.digest(input.getBytes(StandardCharsets.UTF_8));
-                    StringBuilder hexString = new StringBuilder();
-                    for (byte b : hash) {
-                        String hex = Integer.toHexString(0xff & b);
-                        if (hex.length() == 1) hexString.append('0');
-                        hexString.append(hex);
-                    }
-                    return hexString.toString();
-                } catch (NoSuchAlgorithmException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        };
-    }
+			private String hashWithSHA256(String input) {
+				try {
+					MessageDigest md = MessageDigest.getInstance("SHA-256");
+					byte[] hash = md.digest(input.getBytes(StandardCharsets.UTF_8));
+					StringBuilder hexString = new StringBuilder();
+					for (byte b : hash) {
+						String hex = Integer.toHexString(0xff & b);
+						if (hex.length() == 1) hexString.append('0');
+						hexString.append(hex);
+					}
+					return hexString.toString();
+				} catch (NoSuchAlgorithmException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		};
+	}
 
-    @Bean
-    public AuthenticationManager authenticationManager() throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
+	@Bean
+	public AuthenticationManager authenticationManager() throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
 
-    @Bean
-    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.headers(headers -> headers
-                        .defaultsDisabled()
-                        .frameOptions(frame -> frame.sameOrigin())
-                )
-                .authorizeHttpRequests(
-                        authorz -> authorz
-                                .requestMatchers("/user/mypage").authenticated()
-                                .anyRequest().permitAll()
-                )
-                .csrf(csrf -> csrf.disable())
-                .formLogin(form -> form
-                        .loginPage("/login") // 커스텀 로그인 페이지 경로
-                        .loginProcessingUrl("/api/auth/login") // 로그인 form의 action과 일치
-                        .defaultSuccessUrl("/") // 로그인 성공 시 이동할 경로
-                        .permitAll()
-                )
-                .logout(logout -> logout
-                        .logoutUrl("/logout") // 로그아웃 요청 URL
-                        .logoutSuccessUrl("/") // 로그아웃 성공 시 이동할 경로
-                        .invalidateHttpSession(true) // 세션 무효화
-                        .deleteCookies("JSESSIONID") // 쿠키 삭제
-                )
-                .addFilter(corsConfig.corsFilter())
-                .sessionManagement(sessionManagement ->
-                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+	@Bean
+	protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.headers(headers -> headers
+						.defaultsDisabled()
+						.frameOptions(frame -> frame.sameOrigin())
+				)
+				.authorizeHttpRequests(
+						authorz -> authorz
+								.requestMatchers("/user/mypage").authenticated()
+								.anyRequest().permitAll()
+				)
+				.csrf(csrf -> csrf.disable())
+				.formLogin(form -> form
+						.loginPage("/login") // 커스텀 로그인 페이지 경로
+						.loginProcessingUrl("/api/auth/login") // 로그인 form의 action과 일치
+						.defaultSuccessUrl("/") // 로그인 성공 시 이동할 경로
+						.permitAll()
+				)
+				.logout(logout -> logout
+						.logoutUrl("/logout") // 로그아웃 요청 URL
+						.logoutSuccessUrl("/") // 로그아웃 성공 시 이동할 경로
+						.invalidateHttpSession(true) // 세션 무효화
+						.deleteCookies("JSESSIONID") // 쿠키 삭제
+				)
+				.addFilter(corsConfig.corsFilter())
+				.sessionManagement(sessionManagement -> sessionManagement
+						.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+						.maximumSessions(1)
+						.expiredUrl("/login")
+						.and()
+						.invalidSessionUrl("/login")
+						.sessionFixation().newSession()
+				)
+		;
 
-        return http.build();
-    }
+		return http.build();
+	}
 
 }
